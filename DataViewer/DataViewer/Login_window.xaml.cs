250aa39aec266eb872d7c8bd457e411a40cae0a6
datnt <dat10hoa1@gmail.com>
2023-01-12 00:18:49 +0700

    add main window

---
+++ DataViewer/DataViewer/Login_window.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataViewer
+{
+    /// <summary>
+    /// Interaction logic for Login_window.xaml
+    /// </summary>
+    public partial class Login_window : Window
+    {
+        public Login_window()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if ((tbUsername.Text == "dat") && (pbPassword.Password == "123"))
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu");
+            }
+        }
+
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

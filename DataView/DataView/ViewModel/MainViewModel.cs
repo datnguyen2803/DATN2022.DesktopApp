db2ac2a17bf081bc69db3abeaac0f8d8c2a4e6aa
datnt <dat10hoa1@gmail.com>
2023-01-17 23:17:34 +0700

    using .net 6.0 and material design

---
+++ DataView/DataView/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DataView.ViewModel
+{
+    public class MainViewModel
+    {
+        public bool IsLoad = false;
+        public MainViewModel()
+        {
+            if (!IsLoad)
+            {
+                IsLoad = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
